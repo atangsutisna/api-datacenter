0938601a83f0e8335be64251df4bfe9ee83e9f3e
Atang Sutisna <atang.sutisna.87@gmail.com>
2025-05-10 11:26:58 +0700

    convert file to pdf

--- fileconverter.py
+++ fileconverter.py
@@ -1,7 +1,10 @@
-import subprocess
+import subprocess, os
 from pathlib import Path
 
-def pptx_to_pdf(input_path):
+path_to_file = Path("/home/kangatang/git/filegator/repository/atang/file_example_XLS_100.xls")
+filename = path_to_file.stem
+
+def pptx_to_pdf(input_path) -> str:
     input_path = Path(input_path)
     subprocess.run([
         "libreoffice",
@@ -11,4 +14,11 @@ def pptx_to_pdf(input_path):
         str(input_path)
     ])
 
-pptx_to_pdf("/home/kangatang/git/filegator/repository/atang/file_example_XLS_100.xls")
+    return os.path.join(input_path.parent, input_path.stem +".pdf") 
+
+# print(filename)
+# print(os.path.join(path_to_file.parent, filename +".pdf"))
+output = pptx_to_pdf("/home/kangatang/git/filegator/repository/atang/file_example_XLS_100.xls")
+print(f"Output: {output}")
+print("attempting to remove file")
+os.remove(output)
