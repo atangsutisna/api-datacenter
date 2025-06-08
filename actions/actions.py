d6fbb1e670e48411e2c9b0c620592bd11b61e20e
Atang Sutisna <atang.sutisna.87@gmail.com>
2025-06-08 21:55:23 +0700

    fix create a new folder

--- actions/actions.py
+++ actions/actions.py
@@ -640,80 +640,87 @@ class ActionCreateFolder(Action):
         current_path = tracker.get_slot("current_path")
         folder_name = tracker.get_slot("folder_name")
         logger.info("Got slot `folder_name` value: %s", folder_name)
-        dispatcher.utter_message(text=f"Folder '{folder_name}' berhasil dibuat")
-        return [
-            SlotSet("folder_name", None),
-        ]
-        # if current_path is None:
-        #     dispatcher.utter_message(text="Silahkan kamu pilih dulu lokasi folder-nya")
-        #     return []
-
-        # metadata = tracker.latest_message.get("metadata")
-        # fullname = metadata.get("fullname")
-        # telegram_id = metadata.get("telegram_id")
-        # # telegram_id = "7272740693"
-        # creation_permitted = self.is_permitted(telegram_id, current_path, "write")
-        # logger.info("Is telegram id %s has creation permission: %s", telegram_id, creation_permitted)
-        # if creation_permitted is False:
-        #     dispatcher.utter_message(response="utter_permission_denied_create_folder")
-        #     return []
-
-        # """ Ketika nama folder tidak didefinisikan, maka sistem akan membuat folder default"""
-        # # TODO: check the permissions        
-        # if creation_permitted:
-        #     # folder_name has not been set
-        #     lspaths = None
-        #     if folder_name is None:
-        #         counter = 0
-        #         folder_name = "New Folder"
-        #         while True:
-        #             try:
-        #                 full_path = os.path.join(current_path, folder_name)
-        #                 logger.info("attempting to create new folder with name %s", folder_name)
-        #                 os.makedirs(full_path)
-
-        #                 lspaths = self.list_dir(current_path)
-        #                 response = self.build_response(
-        #                     opening_message=f"Folder baru dengan nama '{folder_name}' sudah dibuat",
-        #                     ending_message=get_ask_to_open_remove_or_download(),
-        #                     lspaths=lspaths
-        #                 )
-        #                 # dispatcher.utter_message(response="utter_folder_created_success", folder_name=folder_name, current_path=current_path)
-        #                 dispatcher.utter_message(text=response)
-        #                 break
-        #             except FileExistsError:
-        #                 counter += 1
-        #                 folder_name = f"New Folder {counter}"
-        #             except Exception as e:
-        #                 dispatcher.utter_message(response="utter_create_folder_failed", folder_name=folder_name)
-        #                 traceback.print_exc()
-        #                 break
-        #         # TODO: sebelum direturn, tolong update search_result, agar folder baru masuk di dalamnya
-        #         lspaths_json = json.dumps(self.to_dict(lspaths))
-        #         return [
-        #             SlotSet("search_results", lspaths_json)
-        #         ]
-
-        #     # folder_name has been set
-        #     if folder_name is not None:
-        #         dispatcher.utter_message(text="Folder with requested name has been created")
-        #         return [
-        #             SlotSet("folder_name", None),
-        #             SlotSet("creation_permitted", None)
-        #         ]
-        #     # full_path = os.path.join(current_path, folder_name)
-        #     # try:
-        #     #     os.makedirs(full_path) # Membuat folder
-        #     #     dispatcher.utter_message(response="utter_folder_created_success", folder_name=folder_name, current_path=current_path)
-        #     # except OSError as e: # Tangani error jika terjadi masalah saat membuat folder
-        #     #     dispatcher.utter_message(response="utter_create_folder_failed", folder_name=folder_name)
-        #     #     print(f"Error creating folder '{full_path}': {e}")
-        #     # except Exception as e:
-        #     #     dispatcher.utter_message(response="utter_create_folder_failed", folder_name=folder_name)
+        # dispatcher.utter_message(text=f"Folder '{folder_name}' berhasil dibuat")
         # return [
         #     SlotSet("folder_name", None),
-        #     SlotSet("creation_permitted", None)
         # ]
+
+        if current_path is None:
+            dispatcher.utter_message(text="Silahkan kamu pilih dulu lokasi folder-nya")
+            return []
+
+        metadata = tracker.latest_message.get("metadata")
+        fullname = metadata.get("fullname")
+        telegram_id = metadata.get("telegram_id")
+        # telegram_id = "7272740693"
+        creation_permitted = self.is_permitted(telegram_id, current_path, "write")
+        logger.info("Is telegram id %s has creation permission: %s", telegram_id, creation_permitted)
+        if creation_permitted is False:
+            logger.info("User with id %s is not permitted to write", telegram_id)
+            dispatcher.utter_message(response="utter_permission_denied_create_folder", current_path=current_path)
+            return [
+                SlotSet("folder_name", None),
+                SlotSet("creation_permitted", None)
+            ]
+
+        """ create folder is premitted and folder is None """
+        lspaths = None
+        if folder_name is None:
+            counter = 0
+            folder_name = "New Folder"
+            while True:
+                try:
+                    full_path = os.path.join(current_path, folder_name)
+                    logger.info("attempting to create new folder with name %s", folder_name)
+                    os.makedirs(full_path)
+
+                    lspaths = self.list_dir(current_path)
+                    response = self.build_response(
+                        opening_message=f"Folder baru dengan nama '{folder_name}' sudah dibuat",
+                        ending_message=get_ask_to_open_remove_or_download(),
+                        lspaths=lspaths
+                    )
+                    # dispatcher.utter_message(response="utter_folder_created_success", folder_name=folder_name, current_path=current_path)
+                    dispatcher.utter_message(text=response)
+                    break
+                except FileExistsError:
+                    counter += 1
+                    folder_name = f"New Folder {counter}"
+                except Exception as e:
+                    dispatcher.utter_message(response="utter_create_folder_failed", folder_name=folder_name)
+                    traceback.print_exc()
+                    break
+            # TODO: sebelum direturn, tolong update search_result, agar folder baru masuk di dalamnya
+            lspaths_json = json.dumps(self.to_dict(lspaths))
+            return [
+                SlotSet("search_results", lspaths_json)
+            ]
+
+        """ Ketika nama folder tidak didefinisikan, maka sistem akan membuat folder default"""
+        if folder_name is not None:
+            try:
+                full_path = os.path.join(current_path, folder_name)
+                logger.info("attempting to create new folder with name %s", folder_name)
+                os.makedirs(full_path)
+
+                lspaths = self.list_dir(current_path)
+                response = self.build_response(
+                    opening_message=f"Folder baru dengan nama '{folder_name}' sudah dibuat",
+                    ending_message=get_ask_to_open_remove_or_download(),
+                    lspaths=lspaths
+                )
+                dispatcher.utter_message(text=response)
+            except FileExistsError:
+                dispatcher.utter_message(response="utter_create_folder_failed", folder_name=folder_name)
+                traceback.print_exc()
+            except Exception as e:
+                dispatcher.utter_message(response="utter_create_folder_failed", folder_name=folder_name)
+                traceback.print_exc()
+
+            return [
+                SlotSet("folder_name", None),
+                SlotSet("creation_permitted", None)
+            ]
 
 class ValidateCreateFolderForm(FormValidationAction):
     def __init__(self):
