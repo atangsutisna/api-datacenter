a6d8685fdc3c129447238b68945c36af2bd8534a
Atang Sutisna <atang.sutisna.87@gmail.com>
2025-06-08 23:45:48 +0700

    search file with keyword

--- actions/actions.py
+++ actions/actions.py
@@ -778,6 +778,7 @@ class ValidateCreateFolderForm(FormValidationAction):
 """ action perform search """
 class ActionPerformSearch(Action):
     def __init__(self):
+        from userloggedin import get_user_logged_in
         from checkpermissions import is_permitted
         from myutils import get_root_path, list_dir, to_dict, build_response
         self.is_permitted = is_permitted
@@ -785,6 +786,7 @@ class ActionPerformSearch(Action):
         self.list_dir = list_dir
         self.to_dict = to_dict
         self.build_response = build_response
+        self.get_user_logged_in = get_user_logged_in
 
     def name(self):
         return "action_perform_search"
@@ -792,9 +794,48 @@ class ActionPerformSearch(Action):
     async def run(self, dispatcher: CollectingDispatcher,
                   tracker: Tracker,
                   domain: dict):
+        REPOSITORY_PATH = os.getenv('REPOSITORY_PATH')
         search_query = tracker.get_slot("search_query")
+        # metadata = tracker.latest_message.get("metadata")
+        # fullname = metadata.get("fullname")
+        # telegram_id = metadata.get("telegram_id")
+        telegram_id = "7272740693"
+        curr_user = self.get_user_logged_in(telegram_id)
+        if curr_user is None:
+            dispatcher.utter_message('Maaf, anda belum bisa mengakses data center. Silakan verifikasi nomor HP kamu')
+            return [
+                SlotSet("search_query", None)
+            ]
+
+        results = []
+        accounts = curr_user['accounts']
+        for account in accounts:
+            homedir = account['homedir'].lstrip("/")
+            base_path = os.path.join(REPOSITORY_PATH, homedir)
+            logger.info("attempting to find file with key '%s' on path %s", search_query, base_path)
+            for root, dirs, files in os.walk(base_path):
+                # find a folder with name like keyword
+                for dir_name in dirs:
+                    if search_query.lower() in dir_name.lower():
+                        fullpath = os.path.join(root, dir_name)
+                        results.append(fullpath)
+
+                # find for a file
+                for file in files:
+                    if search_query.lower() in file.lower():
+                        fullpath = os.path.join(root, file)
+                        results.append(fullpath)
+
+        count_search_result = len(results)
+        logger.info("Got result %d for query search %s", count_search_result, search_query)
+        if len(results) == 0:
+            dispatcher.utter_message(f"Saya tidak dapat menemukan file atau data yang mengandung kata `{search_query}` 😞")
+            return [
+                SlotSet("search_query", None)
+            ]
+
         logger.info("attempting to find file or data with keyword: %s", search_query)
-        dispatcher.utter_message(text=f"Oke, saya akan mencari: '{search_query}'.")
+        dispatcher.utter_message(text=f"Oke, saya sudah menemukan file atau data dengan kata kunci: '{search_query}'.")
         return [
             SlotSet("search_query", None)
         ]
