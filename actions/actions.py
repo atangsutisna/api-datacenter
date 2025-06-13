ddebbafbe2426bec73d6d42030829e28ca0a377f
Atang Sutisna <atang.sutisna.87@gmail.com>
2025-06-14 06:06:22 +0700

    remove kalimat sebutkan angkanya

--- actions/actions.py
+++ actions/actions.py
@@ -95,7 +95,7 @@ def simplified_path(original_path: str) -> str:
 def get_ask_to_open_remove_or_download() -> str:
     additional_responses = [
         "Ada hal lain yang perlu saya bantu dengan data ini? Misalnya, Kamu ingin menghapus, mengunduh, atau membuka folder lain?",
-        "Perlu bantuan lanjutan? Saya bisa bantu hapus, unduh, atau masuk ke folder lain. Cukup beritahu nomor yang Kamu inginkan.",
+        "Perlu bantuan lanjutan? Saya bisa bantu hapus, unduh, atau masuk ke folder lain.",
         "Apa lagi yang bisa saya lakukan untuk Kamu? Ada pilihan hapus, unduh, atau jelajahi folder.",
         "Apakah ada tindakan lain yang ingin Kamu lakukan? Misalnya, menghapus, mengunduh, atau membuka folder?",
         "Sudah selesai dengan ini, atau ada lagi yang bisa saya bantu? Mungkin menghapus, mengunduh, atau membuka folder lain?"
