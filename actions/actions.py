328b51de1f27885b2dddd15e41ed6f977b18dd60
Atang Sutisna <atang.sutisna.87@gmail.com>
2025-06-19 06:19:26 +0700

    fix remove data

--- actions/actions.py
+++ actions/actions.py
@@ -555,65 +555,69 @@ class ActionRemoveData(Action):
         logger.info("starting to remove data with new rule")
         file_no = tracker.get_slot("file_no_to_be_removed")
         current_path = tracker.get_slot("current_path")
-        return [
-            SlotSet("file_no_to_be_removed", None),
-            SlotSet("file_no", None)
-        ]
 
-        # metadata = tracker.latest_message.get("metadata")
-        # fullname = metadata.get("fullname")
-        # telegram_id = metadata.get("telegram_id")
+        metadata = tracker.latest_message.get("metadata")
+        fullname = metadata.get("fullname")
+        telegram_id = metadata.get("telegram_id")
 
-        # logger.info("Got current path %s", current_path)
-        # chmod_permitted = self.is_permitted(telegram_id, current_path, "chmod")
-        # if chmod_permitted:
-        #     # check file or folder?
-        #     search_results = tracker.get_slot("search_results")
-        #     user_files = json.loads(search_results)
-        #     logger.info("current user files %r", user_files)
-        #     selected_path = user_files.get(file_no)
-        #     file_exists = os.path.exists(selected_path)
-        #     removed_path = simplified_path(selected_path)
-        #     if selected_path and file_exists:
-        #         is_file = os.path.isfile(selected_path)
-        #         if is_file:         
-        #             # just remove the file    
-        #             os.remove(selected_path)
-        #         else:
-        #             # remove the folder and it's childs
-        #             shutil.rmtree(selected_path)
+        logger.info("Got current path %s", current_path)
+        chmod_permitted = self.is_permitted(telegram_id, current_path, "chmod")
+        if chmod_permitted:
+            # check file or folder?
+            search_results = tracker.get_slot("search_results")
+            user_files = json.loads(search_results)
+            logger.info("current user files %r", user_files)
+            selected_path = user_files.get(file_no)
+            file_exists = os.path.exists(selected_path)
+            removed_path = simplified_path(selected_path)
+            if selected_path and file_exists:
+                is_file = os.path.isfile(selected_path)
+                if is_file:         
+                    # just remove the file    
+                    os.remove(selected_path)
+                else:
+                    # remove the folder and it's childs
+                    shutil.rmtree(selected_path)
                 
-        #         lspaths = self.list_dir(current_path)
-        #         response = self.build_response(
-        #             opening_message=f"Data `{removed_path}` sudah dihapus",
-        #             ending_message=get_ask_to_open_remove_or_download(),
-        #             lspaths=lspaths
-        #         )
-        #         # prepare for response
-        #         dispatcher.utter_message(text=response)
-        #         lspaths_json = json.dumps(self.to_dict(lspaths))
-        #         return [
-        #             SlotSet("search_results", lspaths_json), 
-        #             SlotSet("file_no", None)
-        #         ]
-        #     else:
-        #         # data tidak ditemukan
-        #         lspaths = self.list_dir(current_path)
-        #         response = self.build_response(
-        #             opening_message=f"Data `{removed_path}` nggak ketemu",
-        #             ending_message=get_ask_to_open_remove_or_download(),
-        #             lspaths=lspaths
-        #         )
-        #         # prepare for response
-        #         dispatcher.utter_message(text=response)
-        #         lspaths_json = json.dumps(self.to_dict(lspaths))
-        #         return [
-        #             SlotSet("search_results", lspaths_json), 
-        #             SlotSet("file_no", None)
-        #         ]
-        # else:
-        #     dispatcher.utter_message(text=f"Maaf, kamu nggak ada ijin menghapus")
-        #     return []
+                lspaths = self.list_dir(current_path)
+                response = self.build_response(
+                    opening_message=f"Data `{removed_path}` sudah dihapus",
+                    ending_message=get_ask_to_open_remove_or_download(),
+                    lspaths=lspaths
+                )
+                # prepare for response
+                dispatcher.utter_message(text=response)
+                lspaths_json = json.dumps(self.to_dict(lspaths))
+                return [
+                    SlotSet("search_results", lspaths_json), 
+                    SlotSet("file_no", None),
+                    SlotSet("file_no_to_be_removed", None),
+                    SlotSet("current_path", current_path)
+                ]
+            else:
+                # data tidak ditemukan
+                lspaths = self.list_dir(current_path)
+                response = self.build_response(
+                    opening_message=f"Data `{removed_path}` nggak ketemu",
+                    ending_message=get_ask_to_open_remove_or_download(),
+                    lspaths=lspaths
+                )
+                # prepare for response
+                dispatcher.utter_message(text=response)
+                lspaths_json = json.dumps(self.to_dict(lspaths))
+                return [
+                    SlotSet("search_results", lspaths_json), 
+                    SlotSet("file_no", None),
+                    SlotSet("file_no_to_be_removed", None),
+                    SlotSet("current_path", current_path)
+                ]
+        else:
+            dispatcher.utter_message(text=f"Maaf, kamu nggak ada ijin menghapus")
+            return [
+                SlotSet("file_no", None),
+                SlotSet("file_no_to_be_removed", None),
+                SlotSet("current_path", current_path)
+            ]
 
 class ActionCheckPermissionRemoveData(Action):
     def __init__(self):
