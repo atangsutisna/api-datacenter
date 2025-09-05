cbd05b64b279c965bcbf6031921a46d099b93de5
Atang Sutisna <atang.sutisna.87@gmail.com>
2025-09-05 10:44:40 +0700

    move go home action to go-home file

--- myutils.py
+++ myutils.py
@@ -136,6 +136,13 @@ def read_db(db_path: str):
         db_data = []
     return db_data
 
+def simplified_path(original_path: str) -> str:
+    folder_name = os.path.basename(original_path)
+    repository_path = original_path.split("/repository", 1)[1]
+    
+    parent_path = os.path.dirname(repository_path)
+    return os.path.join(parent_path, folder_name)
+
 # path = "/home/kangatang/git/filegator/repository/spark"
 # lspaths = list_dir(path)
 # formatted_lspaths = format_lspaths(lspaths=lspaths)
