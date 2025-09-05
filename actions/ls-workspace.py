65bb16678afb74c1f6a17a18b87a83c18761e511
Atang Sutisna <atang.sutisna.87@gmail.com>
2025-09-05 15:23:02 +0700

    move ls-workspace

---
+++ actions/ls-workspace.py
@@ -0,0 +1,136 @@
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+from rasa_sdk.events import SlotSet
+import logging,sys,os,random,json
+
+class ActionListWorkspace(Action):
+    def __init__(self):
+        from userloggedin import get_user_logged_in
+        from myutils import simplified_path,get_ask_to_open_remove_or_download
+
+        self.get_user_logged_in = get_user_logged_in
+        self.simplified_path = simplified_path
+        self.get_ask_to_open_remove_or_download = get_ask_to_open_remove_or_download
+
+    def name(self) -> Text:
+        return "action_show_data"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        fullname = tracker.sender_id
+        dispatcher.utter_message(text="Baik, mohon ditunggu...")
+
+        if fullname == "user":
+            # belum login
+            dispatcher.utter_message(text="""
+            Maaf, saya belum mengenal kamu.
+            Silahkan verifikasi nomor HP kamu dulu.
+            """)
+
+            return []
+        else:
+            # get telegram id 7272740693
+            current_path = tracker.get_slot("current_path")
+            if current_path is None:
+                metadata = tracker.latest_message.get("metadata")
+                fullname = metadata.get("fullname")
+                telegram_id = metadata.get("telegram_id")
+                
+                # todo: jika user hanya punya satu folder, tampilkan saja langsung isinya
+                curr_user = self.get_user_logged_in(telegram_id)
+                # todo: check sudah konfirmasi nomor atau belum
+                accounts = curr_user['accounts']
+                user_workspaces = []
+                root_paths = []
+                REPOSITORY_PATH = os.getenv('REPOSITORY_PATH')
+                current_path = None
+                logger.info("total account for user %s : %d", telegram_id, len(accounts))
+                if len(accounts) > 1:
+                    for account in accounts:
+                        homedir = account['homedir'].lstrip("/")
+                        root_path = os.path.join(REPOSITORY_PATH, homedir)
+                        root_paths.append(root_path)
+                        user_workspaces.append(root_path)
+                else:
+                    dirname = accounts[0]['homedir'].lstrip("/")
+                    logger.info("attempting to list all data in %s", dirname)
+                    root_path = os.path.join(REPOSITORY_PATH, dirname)
+                    current_path = root_path
+                    list_dir = os.listdir(root_path)
+                    for dir in list_dir:
+                        child_path = os.path.join(root_path, dir)
+                        root_paths.append(root_path)
+                        user_workspaces.append(child_path)
+
+                opening_messages = [
+                    "Baik, ini semua data yang kamu miliki:",
+                    "Ini daftar data yang kamu miliki:",
+                    "Kamu memiliki beberapa data yang tersimpan. Ini daftarnya:"
+                ]
+                message = random.choice(opening_messages)
+                no = 1
+                search_results = {}
+                for path in user_workspaces:
+                    file = os.path.isfile(path)
+                    simple_path = self.simplified_path(path)
+                    if not file:
+                        message += f"\n{no}. `{simple_path}`"
+                    else:
+                        message += f"\n{no}. `{simple_path}`"
+                    search_results[no] = path
+                    no += 1
+
+                if len(accounts) > 1:
+                    additional_response = "Jika kamu ingin membuka salah satunya, cukup ketik angkanya."
+                else:
+                    additional_response = self.get_ask_to_open_remove_or_download()
+                message += "\n"+ additional_response
+
+                dispatcher.utter_message(
+                    text=message
+                )
+
+                root_paths_json = json.dumps(root_paths)
+                search_results_json = json.dumps(search_results)
+                return [
+                    SlotSet("root_paths", root_paths_json),
+                    SlotSet("search_results", search_results_json),
+                    SlotSet("current_path", current_path)
+                ]
+            else:
+                # tampilkan berdasarkan current path
+                user_workspaces = []
+                list_dir = os.listdir(current_path)
+                for dir in list_dir:
+                    child_path = os.path.join(current_path, dir)
+                    user_workspaces.append(child_path)
+                simplified_root_path = self.simplified_path(current_path)
+                opening_messages = [
+                    f"Baik, ini isi dari folder `{simplified_root_path}`:",
+                    f"Kamu sekarang berada di dalam folder `{simplified_root_path}`. Ini semua yang ada di dalamnya:"
+                ]
+                message = random.choice(opening_messages)
+                no = 1
+                search_results = {}
+                for path in user_workspaces:
+                    file = os.path.isfile(path)
+                    simple_path = self.simplified_path(path)
+                    if not file:
+                        message += f"\n{no}. `{simple_path}`"
+                    else:
+                        message += f"\n{no}. `{simple_path}`"
+                    search_results[no] = path
+                    no += 1
+                additional_response = self.get_ask_to_open_remove_or_download()
+                message += "\n"+ additional_response
+                dispatcher.utter_message(text=message)
+
+                search_results_json = json.dumps(search_results)
+                return [
+                    SlotSet("search_results", search_results_json), 
+                    SlotSet("file_no", None),
+                    SlotSet("current_path", current_path),
+                ]
