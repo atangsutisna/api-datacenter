57b26304757a9459780b374437ebb67b19723fde
Atang Sutisna <atang.sutisna.87@gmail.com>
2025-09-05 15:29:25 +0700

    split download action

---
+++ actions/download.py
@@ -0,0 +1,78 @@
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import logging,sys,os,random,json
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
+)
+logger = logging.getLogger(__name__)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+class ActionPerformDownload(Action):
+    def __init__(self):
+        from userloggedin import get_user_logged_in
+        from checkpermissions import is_permitted
+        from myutils import get_root_path, list_dir, to_dict, build_response,simplified_path
+
+        self.is_permitted = is_permitted
+        self.get_root_path = get_root_path
+        self.list_dir = list_dir
+        self.to_dict = to_dict
+        self.build_response = build_response
+        self.get_user_logged_in = get_user_logged_in
+        self.simplified_path = simplified_path
+
+    def name(self):
+        return "action_perform_download"
+
+    async def run(self, dispatcher: CollectingDispatcher,
+                  tracker: Tracker,
+                  domain: dict):
+        file_no = tracker.get_slot("file_no")
+        logger.info("attempting to send file no %s", file_no)
+        search_results = tracker.get_slot("search_results")
+        # TODO: check download
+        if search_results:
+            user_files = json.loads(search_results)
+            selected_path = user_files.get(file_no)
+            # check permission
+            logger.info("Got the file on path %s", selected_path)
+            metadata = tracker.latest_message.get("metadata")
+            fullname = metadata.get("fullname")
+            telegram_id = metadata.get("telegram_id")
+            download_permitted = self.is_permitted(telegram_id, selected_path, "download")
+            logger.info("Is telegram id %s has write permission: %s", telegram_id, download_permitted)
+            if download_permitted is False:
+                dispatcher.utter_message("Maaf, kamu tidak diijinkan untuk mendownload file disini.")
+                return {
+                    "folder_name": None, 
+                    "creation_permitted": False,
+                    "requested_slot": None
+                }
+
+            is_file = os.path.isfile(selected_path)
+            if is_file:
+                # preparing for download
+                simple_root_path = self.simplified_path(selected_path)
+                file_name = os.path.basename(selected_path)
+                dispatcher.utter_message(
+                    text="Baik, mohon ditunggu...",
+                    attachment={
+                        "type": "file",
+                        "payload": {
+                            "url": selected_path,
+                            "title": "Download",
+                            "name": file_name
+                        }
+                    }
+                )
+            else:
+                # create file attachment
+                dispatcher.utter_message(text="Saya sedang menyiapkan file download untuk kamu")
+        else:
+            dispatcher.utter_message(text="Saya tidak menemukan data tersebut")
+
+        return [
+            SlotSet("file_no", None)
+        ]
