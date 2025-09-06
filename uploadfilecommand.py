a64c15eabf6e9b53f6be3e76e83fef5578c696ba
Atang Sutisna <atang.sutisna.87@gmail.com>
2025-09-06 17:43:16 +0700

    is upload permitted

--- uploadfilecommand.py
+++ uploadfilecommand.py
@@ -58,7 +58,7 @@ async def start_upload_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
             telegram_id = str(update.effective_user.id)
             context.user_data['current_directory'] = current_path
             upload_permitted = is_permitted(telegram_id, current_path, "upload")
-            logger.info("is %s permitted to upload to path %s", curr_user, current_path)
+            logger.info("is %s permitted to upload to path %s : %s ", telegram_id, current_path, upload_permitted)
             if not upload_permitted:
                 await update.message.reply_text("Mohon maaf, kamu tidak punya ijin untuk melakukan upload.")
                 return
