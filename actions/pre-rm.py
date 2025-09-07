3c7fcebe8bd76f920db0bb732cbc2dec48f0f1f2
Atang Sutisna <atang.sutisna.87@gmail.com>
2025-09-07 07:10:44 +0700

    fix check permission before to remove

---
+++ actions/pre-rm.py
@@ -0,0 +1,45 @@
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+from rasa_sdk.events import SlotSet
+import logging,sys,os,random,json
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
+)
+logger = logging.getLogger(__name__)
+
+class ActionPrepareRemove(Action):
+    def __init__(self):
+        from checkpermissions import is_permitted
+
+        self.is_permitted = is_permitted
+
+    def name(self) -> Text:
+        return "action_prepare_remove"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        logger.info("starting to prepare rm action...")
+        metadata = tracker.latest_message.get("metadata")
+        fullname = metadata.get("fullname")
+        telegram_id = metadata.get("telegram_id")
+
+        file_no = tracker.get_slot("file_no_to_be_removed")
+        search_results = tracker.get_slot("search_results")
+
+        # logger.info("attempting to load search results %s", search_results)
+        user_files = json.loads(search_results)
+        logger.info("attempting to get path on line %s", file_no)
+        selected_path = user_files.get(file_no)
+
+        has_permission = self.is_permitted(telegram_id, selected_path, "write")
+        logger.info("Is telegram id %s has rm permission: %s", telegram_id, has_permission)
+        if not has_permission:
+            logger.info("user %s has no permission to remove", telegram_id)
+            return [SlotSet("has_rm_permission", False)]
+        
+        logger.info("ok, user %s has permission to remove", telegram_id)
+        return [SlotSet("has_rm_permission", True)]
