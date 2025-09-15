9b7200b203826c97ae83683e0c74f27d00ac3cbb
Atang Sutisna <atang.sutisna.87@gmail.com>
2025-09-16 06:00:05 +0700

    fix remove multi file or folder

--- actions/rm.py
+++ actions/rm.py
@@ -14,13 +14,14 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 class ActionRemoveData(Action):
     def __init__(self):
         from checkpermissions import is_permitted
-        from myutils import list_dir,build_response,to_dict,simplified_path,get_ask_to_open_remove_or_download
+        from myutils import list_dir,build_response,to_dict,simplified_path,get_ask_to_open_remove_or_download,to_be_list
         self.is_permitted = is_permitted
         self.list_dir = list_dir
         self.build_response = build_response
         self.to_dict = to_dict
         self.simplified_path = simplified_path
         self.get_ask_to_open_remove_or_download = get_ask_to_open_remove_or_download
+        self.to_be_list = to_be_list
         
     def name(self):
         return "action_remove_data"
@@ -51,50 +52,81 @@ class ActionRemoveData(Action):
             search_results = tracker.get_slot("search_results")
             user_files = json.loads(search_results)
             # logger.info("current user files %r", user_files)
-            selected_path = user_files.get(file_no)
-            file_exists = os.path.exists(selected_path)
-            removed_path = self.simplified_path(selected_path)
-            if selected_path and file_exists:
-                is_file = os.path.isfile(selected_path)
-                if is_file:         
-                    # just remove the file    
-                    os.remove(selected_path)
+            
+            for file_no in delete_indexes:
+                selected_path = user_files.get(file_no)
+                file_exists = os.path.exists(selected_path)
+                if file_exists:
+                    removed_path = self.simplified_path(selected_path)
+                    is_file = os.path.isfile(selected_path)
+                    if is_file:         
+                        logger.info("attempting to remove file on path %s", selected_path)
+                        # just remove the file    
+                        os.remove(selected_path)
+                    else:
+                        logger.info("attempting to remove folder on path %s", selected_path)
+                        # remove the folder and it's childs
+                        shutil.rmtree(selected_path)
                 else:
-                    # remove the folder and it's childs
-                    shutil.rmtree(selected_path)
+                    logger.info("file number %s not found")
                 
-                lspaths = self.list_dir(current_path)
-                response = self.build_response(
-                    opening_message=f"Data `{removed_path}` sudah dihapus",
-                    ending_message=self.get_ask_to_open_remove_or_download(),
-                    lspaths=lspaths
-                )
-                # prepare for response
-                dispatcher.utter_message(text=response)
-                lspaths_json = json.dumps(self.to_dict(lspaths))
-                return [
-                    SlotSet("search_results", lspaths_json), 
-                    SlotSet("file_no", None),
-                    SlotSet("file_no_to_be_removed", None),
-                    SlotSet("current_path", current_path)
-                ]
-            else:
-                # data tidak ditemukan
-                lspaths = self.list_dir(current_path)
-                response = self.build_response(
-                    opening_message=f"Data `{removed_path}` nggak ketemu",
-                    ending_message=self.get_ask_to_open_remove_or_download(),
-                    lspaths=lspaths
-                )
-                # prepare for response
-                dispatcher.utter_message(text=response)
-                lspaths_json = json.dumps(self.to_dict(lspaths))
-                return [
-                    SlotSet("search_results", lspaths_json), 
-                    SlotSet("file_no", None),
-                    SlotSet("file_no_to_be_removed", None),
-                    SlotSet("current_path", current_path)
-                ]
+            lspaths = self.list_dir(current_path)
+            response = self.build_response(
+                opening_message=f"Data sudah dihapus",
+                ending_message=self.get_ask_to_open_remove_or_download(),
+                lspaths=lspaths
+            )
+            # prepare for response
+            dispatcher.utter_message(text=response)
+            lspaths_json = json.dumps(self.to_dict(lspaths))
+            return [
+                SlotSet("search_results", lspaths_json), 
+                SlotSet("file_no", None),
+                SlotSet("file_no_to_be_removed", None),
+                SlotSet("current_path", current_path)
+            ]
+        
+            # if selected_path and file_exists:
+            #     is_file = os.path.isfile(selected_path)
+            #     if is_file:         
+            #         # just remove the file    
+            #         os.remove(selected_path)
+            #     else:
+            #         # remove the folder and it's childs
+            #         shutil.rmtree(selected_path)
+                
+            #     lspaths = self.list_dir(current_path)
+            #     response = self.build_response(
+            #         opening_message=f"Data `{removed_path}` sudah dihapus",
+            #         ending_message=self.get_ask_to_open_remove_or_download(),
+            #         lspaths=lspaths
+            #     )
+            #     # prepare for response
+            #     dispatcher.utter_message(text=response)
+            #     lspaths_json = json.dumps(self.to_dict(lspaths))
+            #     return [
+            #         SlotSet("search_results", lspaths_json), 
+            #         SlotSet("file_no", None),
+            #         SlotSet("file_no_to_be_removed", None),
+            #         SlotSet("current_path", current_path)
+            #     ]
+            # else:
+            #     # data tidak ditemukan
+            #     lspaths = self.list_dir(current_path)
+            #     response = self.build_response(
+            #         opening_message=f"Data `{removed_path}` nggak ketemu",
+            #         ending_message=self.get_ask_to_open_remove_or_download(),
+            #         lspaths=lspaths
+            #     )
+            #     # prepare for response
+            #     dispatcher.utter_message(text=response)
+            #     lspaths_json = json.dumps(self.to_dict(lspaths))
+            #     return [
+            #         SlotSet("search_results", lspaths_json), 
+            #         SlotSet("file_no", None),
+            #         SlotSet("file_no_to_be_removed", None),
+            #         SlotSet("current_path", current_path)
+            #     ]
         else:
             dispatcher.utter_message(text=f"Maaf, kamu nggak ada ijin menghapus")
             return [
