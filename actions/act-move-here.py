813e2707bb534aa48f7a66c36c7866808cfce3a6
Atang Sutisna <atang.sutisna.87@gmail.com>
2025-11-01 11:52:07 +0700

    save here

---
+++ actions/act-move-here.py
@@ -0,0 +1,46 @@
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+from rasa_sdk.events import SlotSet
+import logging,sys,os,random,json
+import traceback
+
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
+)
+logger = logging.getLogger(__name__)
+
+class ActionMoveHere(Action):
+    def __init__(self):
+        from checkpermissions import is_permitted
+        from myutils import to_be_list
+        
+        self.is_permitted = is_permitted
+        self.to_be_list = to_be_list
+
+    def name(self) -> Text:
+        return "action_move_here"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        logger.info("starting to move action...")
+        metadata = tracker.latest_message.get("metadata")
+        fullname = metadata.get("fullname")
+        telegram_id = metadata.get("telegram_id")
+
+        source_path = tracker.get_slot("source_path")
+        if source_path is None:
+            dispatcher.utter_message(text=f"Kamu belum memilih file atau folder.")
+            return []
+        else:
+            current_path = tracker.get_slot("current_path")
+
+            logger.info("attempting to move %s to %s", source_path, current_path)
+            dispatcher.utter_message(text=f"File {source_path} sudah dipindahkan ke {current_path}")
+            return [
+                SlotSet("source_path", None),
+                SlotSet("source_file_no", None)
+            ]
