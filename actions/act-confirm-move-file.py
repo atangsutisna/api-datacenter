b0e85378e001e5425c0fe50898ff0f102b84e7ee
Atang Sutisna <atang.sutisna.87@gmail.com>
2025-11-01 09:42:19 +0700

    prepare intent and action for moving a file

---
+++ actions/act-confirm-move-file.py
@@ -0,0 +1,56 @@
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+from rasa_sdk.events import SlotSet
+import logging,sys,os,random,json
+import traceback
+
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
+)
+logger = logging.getLogger(__name__)
+
+class ActionPrepareRemove(Action):
+    def __init__(self):
+        from checkpermissions import is_permitted
+        from myutils import to_be_list
+        
+        self.is_permitted = is_permitted
+        self.to_be_list = to_be_list
+
+    def name(self) -> Text:
+        return "action_confirm_move_file"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        logger.info("starting to prepare move action...")
+        metadata = tracker.latest_message.get("metadata")
+        fullname = metadata.get("fullname")
+        telegram_id = metadata.get("telegram_id")
+
+        file_no = tracker.get_slot("source_file_no")
+        # search_results = tracker.get_slot("search_results")
+
+        # user_files = json.loads(search_results)
+        # has_permission = True
+        # for file_no in delete_indexes:
+        #     logger.info("attempting to get path on line %s", file_no)
+        #     selected_path = user_files.get(file_no)
+        #     if selected_path:
+        #         has_permission = has_permission and self.is_permitted(telegram_id, selected_path, "write")
+        #     else:
+        #         logger.info("Failed to find path on line %s", file_no)
+    
+        # logger.info("Is telegram id %s has rm permission: %s", telegram_id, has_permission)
+        # if not has_permission:
+        #     logger.info("user %s has no permission to remove", telegram_id)
+        #     return [SlotSet("has_rm_permission", False)]
+        
+        # logger.info("ok, user %s has permission to remove", telegram_id)
+
+        logger.info("attempting to move file with no %s", file_no)
+        dispatcher.utter_message(text=f"Baik, saya akan pindahkan file ini: {file_no}")
+        return []
