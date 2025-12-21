7ba6c7d1e0d9d95bb495c5d987c5db17ef6154f2
Atang Sutisna <atang.sutisna.87@gmail.com>
2025-12-21 21:19:41 +0700

    update pesan sesi berakhir

--- main.py
+++ main.py
@@ -175,13 +175,13 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
             if has_expired(session_expiry):
                 logger.info("your session has expired")
                 await update.message.reply_text(
-                    "Sesi Anda telah berakhir. Silakan masukkan PIN kembali untuk verifikasi.",
+                    "Sesi Anda telah berakhir. Gunakan /pin untuk mulai sesi baru.",
                     parse_mode="Markdown"
                 )
-                await update.message.reply_text(
-                    "Gunakan /pin untuk mulai sesi baru",
-                    parse_mode="Markdown"
-                )
+                # await update.message.reply_text(
+                #     "Gunakan /pin untuk mulai sesi baru",
+                #     parse_mode="Markdown"
+                # )
                 return
 
     # get telegram id
